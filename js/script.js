e6ba4dd0f5390824d48df762d6106f5fe9628b52
Vladislav <vladislav-gering@mail.ru>
2022-09-22 13:56:18 +0300

    modification with callback button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    //табы
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent  = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader');
+
 
     function hideTabContent(){
         tabsContent.forEach(item => {
@@ -38,5 +39,121 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    //TImer
+    const deadline = '2023-10-19';
+
+    function getTimeRemaining(endtime){
+        let days, hours, minutes,seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date()); //окличесчестов млс
+              if (t <= 0){
+                days = 0; 
+                hours = 0;
+                minutes = 0;
+                seconds = 0;
+              }else {
+                days = Math.floor(t / (1000*60*60*24)),
+              hours = Math.floor((t / (1000*60*60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60 ),
+              seconds = Math.floor((t / 1000) % 60);
+              }
+              
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        }else {
+                return num;
+            }
+    } 
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              temeInterval = setInterval(updateClock,1000);
+        //убрать мигание верстки при перзагрузки страницы
+        updateClock(); //функция инициализации
+
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+//остановка таймера
+            if (t.total <= 0){
+                clearInterval(temeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+    //modal window
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        // modal.classList.toggle('show');
+        document.body.style.overflow = 'hiden'; //отмена прокрутки страницы, когда открыто модалное окно
+        clearInterval(modalTimerId); //если пользователь сам нажал на модальное
+    }
+
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    //hпи кллике на подложку закрывается модальное окно
+    modal.addEventListener('click', (e)=> {
+        if (e.target === modal){
+            closeModal();
+        }
+    });
+
+    //закрытие при нажатие на esc
+    document.addEventListener('keydown', (e)=>{
+        if (e.code === "Escape" && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
+
+    //открытие модального окна автоматически через некоторок
+    const modalTimerId = setTimeout(openModal,5000);
+
+    function showModalByScroll(){
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);//удаляем обработчик, чтобы повторно не открывалось при пролистовании вниз
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll); 
 
 });
