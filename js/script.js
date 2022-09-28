c7a24004a0333e72619209a546d32c21a473cbea
Vladislav <vladislav-gering@mail.ru>
2022-09-28 22:43:41 +0300

    fetch API with promis

--- js/script.js
+++ js/script.js
@@ -256,11 +256,6 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -268,19 +263,24 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
 
-            request.send(json);
 
-            request.addEventListener('load', ()=> {
-                if (request.status === 200){
-                    console.log(request.response);
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset(); //очистка формы
                     statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset(); //очистка формы
             });
 
         });
@@ -310,8 +310,5 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-            .then(response => response.json())
-            .then(json => console.log(json));
-            
+
 });
