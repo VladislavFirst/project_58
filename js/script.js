bba9010646d31897898cd9b62ce8e8f8559c05e3
Vladislav <vladislav-gering@mail.ru>
2022-09-28 22:25:39 +0300

    modal window with effects(no promis)

--- js/script.js
+++ js/script.js
@@ -105,8 +105,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //modal window
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalCloseBtn = document.querySelector('[data-close]');
+          modal = document.querySelector('.modal');
 
     function openModal(){
         modal.classList.add('show');
@@ -129,11 +128,9 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    modalCloseBtn.addEventListener('click', closeModal);
-
     //hпи кллике на подложку закрывается модальное окно
     modal.addEventListener('click', (e)=> {
-        if (e.target === modal){
+        if (e.target === modal || e.target.getAttribute('data-close') == ''){
             closeModal();
         }
     });
@@ -146,7 +143,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     //открытие модального окна автоматически через некоторок
-    // const modalTimerId = setTimeout(openModal,5000);
+    const modalTimerId = setTimeout(openModal,5000);
 
     function showModalByScroll(){
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){
@@ -233,5 +230,88 @@ window.addEventListener('DOMContentLoaded', () => {
         'menu__item'
     ).render();
     
+    //forms
 
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'спасибо! Скоро мы с тобой свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form){
+        form.addEventListener('submit', (e)=> {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value,key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', ()=> {
+                if (request.status === 200){
+                    console.log(request.response);
+                    showThanksModal(message.success);
+                    form.reset(); //очистка формы
+                    statusMessage.remove();
+                } else {
+                    showThanksModal(message.failure);
+                }
+            });
+
+        });
+    }
+
+    function showThanksModal(message){
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML=`
+            <div class="modal__dialog">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() =>{
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => console.log(json));
+            
 });
